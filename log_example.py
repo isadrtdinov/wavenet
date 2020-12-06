9c04fa4fbbe35e6e6b095d2f3bd0d448e50be291
Ubuntu <isadrtdinov@isadrtdinovDL.11p2tmnjmz3ubowvwanmq5kfua.bx.internal.cloudapp.net>
2020-12-06 19:56:06 +0000

    fix example logging

--- log_example.py
+++ log_example.py
@@ -8,12 +8,16 @@ def main():
     params = set_params()
     wandb.init(project=params.wandb_project)
 
-    ground_truth = torchaudio.load(params.ground_truth_audio).squeeze(0).numpy()
-    generated = torchaudio.load(params.example_audio).squeeze(0).numpy()
-    spectrogram = torch.load(params.example_spectrogram).squeeze(0).numpy()
+    ground_truth, _ = torchaudio.load(params.ground_truth_audio)
+    generated, _ = torchaudio.load(params.example_audio)
+    spectrogram = torch.load(params.example_spectrogram)
 
-    wandb.log({'ground truth audio': wandb.Audio(ground_truth),
-               'generated audio': wandb.Audio(generated),
+    ground_truth = ground_truth.squeeze(0).numpy()
+    generated = generated.squeeze(0).numpy()
+    spectrogram = spectrogram.squeeze(0).numpy()
+
+    wandb.log({'ground truth audio': wandb.Audio(ground_truth, sample_rate=params.sample_rate),
+               'generated audio': wandb.Audio(generated, sample_rate=params.sample_rate),
                'spectrogram': wandb.Image(spectrogram)})
 
 
